4dfcd9876346ab551f08e0f1fd79cc22c15f0a50
2046 <g@ggg.gd>
2022-06-06 16:54:50 +0800

    style: purifying code

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,19 @@
 import chalk from 'chalk'
-import { basename } from 'path'
-import download from './download'
-import { exec } from 'child_process'
-import { IPackOpition, IListrContext } from './types'
+import rimraf from 'rimraf'
+import { tmpdir } from './utils'
+import { promisify } from 'util'
+import { IListrContext } from './types'
 import { Listr, ListrTaskWrapper, ListrDefaultRenderer } from 'listr2'
-import {
-  isPackFile,
-  loadFile,
-  parse,
-  tmpdir,
-  decrypt,
-  isAppType,
-  isAppleCPU,
-  isCommandType,
-  calcPercent
-} from './utils'
+import { getConfigTasks, getInstallAppsTasks, getCommandTasks } from './tasks'
 
 const [filePath = '', password = ''] = process.argv.slice(2)
-const fileName = basename(filePath)
 
 ;(async function () {
-  const configTasks = [
-    {
-      title: 'Verify config file format',
-      task: () => {
-        if (!isPackFile(filePath)) {
-          throw new Error(chalk.red('The file format is incorrect, please use the .pack file format.'))
-        }
-      }
-    },
-    {
-      title: `Loading ${fileName} file`,
-      task: async (ctx: IListrContext) => {
-        try {
-          ctx.text = await loadFile(filePath)
-        } catch (error) {
-          throw new Error(chalk.red((<Error>error).message))
-        }
-      }
-    },
-    {
-      title: `Decrypt ${fileName} file`,
-      skip: () => !password,
-      task: (ctx: IListrContext) => {
-        try {
-          ctx.text = decrypt(ctx.text, password)
-        } catch (error) {
-          throw new Error(chalk.red((<Error>error).message))
-        }
-      }
-    },
-    {
-      title: `Parsing ${fileName} file`,
-      task: (ctx: IListrContext) => {
-        try {
-          ctx.tasks = parse<Array<IPackOpition>>(ctx.text)
-        } catch (error) {
-          throw new Error(chalk.red((<Error>error).message))
-        }
-      }
-    }
-  ]
-
   const tasks = [
     {
       title: 'Load Config File',
       task: (_: IListrContext, task: ListrTaskWrapper<IListrContext, ListrDefaultRenderer>) => {
-        return task.newListr(configTasks)
+        return task.newListr(getConfigTasks(filePath, password))
       }
     },
     {
@@ -82,71 +29,25 @@ const fileName = basename(filePath)
     {
       title: 'Install Apps',
       task: (ctx: IListrContext, task: ListrTaskWrapper<IListrContext, ListrDefaultRenderer>) => {
-        return task.newListr(
-          ctx.tasks
-            .filter((task) => isAppType(task))
-            .map((app) => {
-              const title = app.name
-              const url = getDownloadUrl(app.downloadUrl)
-
-              return {
-                title,
-                task: (_: IListrContext, task: ListrTaskWrapper<IListrContext, ListrDefaultRenderer>) => {
-                  return task.newListr([
-                    {
-                      title: 'Downloading',
-                      task: async (ctx: IListrContext, task: ListrTaskWrapper<IListrContext, ListrDefaultRenderer>) => {
-                        try {
-                          let rate = 0
-
-                          const filePath = await execDownload(url, ctx.tmpdir, (chunk, size) => {
-                            task.title = `Downloading [${calcPercent((rate += chunk.length), size)}%]`
-                          })
-
-                          task.title = 'Downloaded'
-                          ctx.filePaths.set(title, filePath)
-                        } catch (error) {
-                          throw new Error(chalk.red((<Error>error).message))
-                        }
-                      }
-                    },
-                    {
-                      title: 'Installing',
-                      task: (_: IListrContext, task: ListrTaskWrapper<IListrContext, ListrDefaultRenderer>) => {
-                        task.title = `Installed dir: ${<string>ctx.filePaths.get(title)}`
-                      }
-                    }
-                  ])
-                }
-              }
-            })
-        )
+        return task.newListr(getInstallAppsTasks(ctx))
       }
     },
     {
-      title: 'Execute Commands',
+      title: 'Change System Settings',
       task: (ctx: IListrContext, task: ListrTaskWrapper<IListrContext, ListrDefaultRenderer>) => {
-        return task.newListr(
-          ctx.tasks
-            .filter((task) => isCommandType(task))
-            .map((command) => {
-              const title = command.description
-              const cmds = Array.isArray(command.cmd) ? command.cmd : [command.cmd]
-
-              return {
-                title,
-                task: async () => {
-                  try {
-                    for (const cmd of cmds) {
-                      await execCommand(cmd)
-                    }
-                  } catch (error) {
-                    throw new Error(chalk.red((<Error>error).message))
-                  }
-                }
-              }
-            })
-        )
+        return task.newListr(getCommandTasks(ctx))
+      }
+    },
+    {
+      title: 'Cleaning Temporary Download Directory',
+      task: async (ctx: IListrContext) => {
+        try {
+          if (ctx.tmpdir) {
+            await promisify(rimraf)(ctx.tmpdir)
+          }
+        } catch (error) {
+          throw new Error(chalk.red((<Error>error).message))
+        }
       }
     }
   ]
@@ -169,33 +70,3 @@ const fileName = basename(filePath)
     error
   }
 })()
-
-function execDownload(url: string, dir: string, onProgress: (chunk: Buffer, size: number) => void) {
-  return new Promise<string>((resolve, reject) => {
-    download({
-      url,
-      dir,
-      onProgress,
-      onError: (error) => reject(error),
-      onComplete: (filePath) => resolve(filePath)
-    })
-  })
-}
-
-function execCommand(command: string) {
-  return new Promise((resolve, reject) => {
-    exec(command, (error, stdout, stderr) => (error ? reject(error) : resolve({ stdout, stderr })))
-  })
-}
-
-function getDownloadUrl(url: string | Array<Array<string>>) {
-  if (Array.isArray(url)) {
-    if (isAppleCPU()) {
-      return url.filter((item) => item[0] === 'arm')[0][1]
-    } else {
-      return url.filter((item) => item[0] === 'intel')[0][1]
-    }
-  } else {
-    return url
-  }
-}
---
+++ src/tasks.ts
@@ -0,0 +1,143 @@
+import chalk from 'chalk'
+import { basename } from 'path'
+import download from './download'
+import { exec } from 'child_process'
+import { ListrTaskWrapper, ListrDefaultRenderer } from 'listr2'
+import { IPackOpition, IListrContext, ProgressEvent } from './types'
+import { isPackFile, decrypt, parse, loadFile, isAppType, isCommandType, isAppleCPU } from './utils'
+
+export function getConfigTasks(filePath: string, password: string) {
+  const fileName = basename(filePath)
+
+  return [
+    {
+      title: 'Verify config file format',
+      task: () => {
+        if (!isPackFile(filePath)) {
+          throw new Error(chalk.red('The file format is incorrect, please use the .pack file format.'))
+        }
+      }
+    },
+    {
+      title: `Loading ${fileName} file`,
+      task: async (ctx: IListrContext) => {
+        try {
+          ctx.text = await loadFile(filePath)
+        } catch (error) {
+          throw new Error(chalk.red((<Error>error).message))
+        }
+      }
+    },
+    {
+      title: `Decrypt ${fileName} file`,
+      skip: () => !password,
+      task: (ctx: IListrContext) => {
+        try {
+          ctx.text = decrypt(ctx.text, password)
+        } catch (error) {
+          throw new Error(chalk.red((<Error>error).message))
+        }
+      }
+    },
+    {
+      title: `Parsing ${fileName} file`,
+      task: (ctx: IListrContext) => {
+        try {
+          ctx.tasks = parse<Array<IPackOpition>>(ctx.text)
+        } catch (error) {
+          throw new Error(chalk.red((<Error>error).message))
+        }
+      }
+    }
+  ]
+}
+
+export function getInstallAppsTasks(ctx: IListrContext) {
+  return ctx.tasks
+    .filter((task) => isAppType(task))
+    .map((app) => {
+      const title = app.name
+      const url = getDownloadUrl(app.downloadUrl)
+
+      return {
+        title,
+        task: (_: IListrContext, task: ListrTaskWrapper<IListrContext, ListrDefaultRenderer>) => {
+          return task.newListr([
+            {
+              title: 'Downloading',
+              task: async (ctx: IListrContext, task: ListrTaskWrapper<IListrContext, ListrDefaultRenderer>) => {
+                try {
+                  const filePath = await execDownload(url, ctx.tmpdir, ({ percent }) => {
+                    task.title = `Downloading [${percent}%]`
+                  })
+
+                  task.title = 'Downloaded'
+                  ctx.filePaths.set(title, filePath)
+                } catch (error) {
+                  throw new Error(chalk.red((<Error>error).message))
+                }
+              }
+            },
+            {
+              title: 'Installing',
+              task: (_: IListrContext, task: ListrTaskWrapper<IListrContext, ListrDefaultRenderer>) => {
+                task.title = `Installed dir: ${<string>ctx.filePaths.get(title)}`
+              }
+            }
+          ])
+        }
+      }
+    })
+}
+
+export function getCommandTasks(ctx: IListrContext) {
+  return ctx.tasks
+    .filter((task) => isCommandType(task))
+    .map((command) => {
+      const title = command.description
+      const cmds = Array.isArray(command.cmd) ? command.cmd : [command.cmd]
+
+      return {
+        title,
+        task: async () => {
+          try {
+            for (const cmd of cmds) {
+              await execCommand(cmd)
+            }
+          } catch (error) {
+            throw new Error(chalk.red((<Error>error).message))
+          }
+        }
+      }
+    })
+}
+
+function execDownload(url: string, dir: string, onProgress: (progressEvent: ProgressEvent) => void) {
+  return new Promise<string>((resolve, reject) => {
+    download({
+      url,
+      dir,
+      onProgress,
+      onError: (error) => reject(error),
+      onComplete: (filePath) => resolve(filePath)
+    })
+  })
+}
+
+function execCommand(command: string) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => (error ? reject(error) : resolve({ stdout, stderr })))
+  })
+}
+
+function getDownloadUrl(url: string | Array<Array<string>>) {
+  if (Array.isArray(url)) {
+    if (isAppleCPU()) {
+      return url.filter((item) => item[0] === 'arm')[0][1]
+    } else {
+      return url.filter((item) => item[0] === 'intel')[0][1]
+    }
+  } else {
+    return url
+  }
+}
