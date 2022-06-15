4ba5cde45067475280f62533aadc469edd90b473
2046 <g@ggg.gd>
2022-06-15 16:42:05 +0800

    feat: crypto tools optimize

--- src/bin/crypto.ts
+++ src/bin/crypto.ts
@@ -1,27 +1,18 @@
-import chalk from 'chalk'
 import { encrypt, decrypt, isPackFile, loadFile } from './utils'
 
 export default {
   async encrypt(filePath: string, password: string) {
-    try {
-      if (!isPackFile(filePath)) {
-        return console.log(chalk.red('The file format is incorrect, please use the .pack file format.'))
-      }
-
-      console.log(encrypt(await loadFile(filePath), password))
-    } catch (error) {
-      console.log(chalk.red(error))
+    if (!isPackFile(filePath)) {
+      throw new Error('The file format is incorrect, please use the .pack file format.')
     }
+
+    return encrypt(await loadFile(filePath), password)
   },
   async decrypt(filePath: string, password: string) {
-    try {
-      if (!isPackFile(filePath)) {
-        return console.log(chalk.red('The file format is incorrect, please use the .pack file format.'))
-      }
-
-      console.log(decrypt(await loadFile(filePath), password))
-    } catch (error) {
-      console.log(chalk.red(error))
+    if (!isPackFile(filePath)) {
+      throw new Error('The file format is incorrect, please use the .pack file format.')
     }
+
+    return decrypt(await loadFile(filePath), password)
   }
 }
