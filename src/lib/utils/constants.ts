6c19072f2b1d0141ff68082d480a78b81796b07f
2046 <g@ggg.gd>
2022-07-22 01:27:31 +0800

    perf: update mas download link, improve download speed

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -2,4 +2,4 @@ import os from 'os'
 
 export const isAppleCPU = os.cpus()[0].model.includes('Apple')
 export const Homebrew_DIR = isAppleCPU ? '/opt/homebrew' : '/usr/local/Homebrew'
-export const MAS_PKG_URL = 'https://github.com/mas-cli/mas/releases/download/v1.8.6/mas.pkg'
+export const MAS_PKG_URL = 'https://cdn.jsdelivr.net/gh/2046/onekey@main/assets/mas.pkg'
