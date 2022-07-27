d57d1deeb59a511bbf48107713d3a79bfa096aef
2046 <g@ggg.gd>
2022-07-28 01:42:11 +0800

    fix: homebrew install app permission denied

--- src/lib/unpack/brew.ts
+++ src/lib/unpack/brew.ts
@@ -5,10 +5,17 @@ export default {
   installer: async function (filePath: string, dest: string) {
     try {
       const binPath = `/usr/local/bin/brew`
+      const whoami = exec('whoami').trim()
 
       await execute(`mkdir -p ${dest} && chmod 777 ${dest}`)
       await decompress(filePath, dest, { strip: 1 })
-      await execute(`ln -s ${dest}/bin/brew ${binPath}`)
+      await execute(`
+        ln -s ${dest}/bin/brew ${binPath} &&
+        chown -R ${whoami} /usr/local/bin /usr/local/include /usr/local/lib /usr/local/share /usr/local/share/doc /usr/local/share/man /usr/local/share/man/man1 &&
+        chmod u+w /usr/local/bin /usr/local/include /usr/local/lib /usr/local/share /usr/local/share/doc /usr/local/share/man /usr/local/share/man/man1 &&
+        mkdir -p /usr/local/Cellar /usr/local/Frameworks /usr/local/etc /usr/local/opt /usr/local/sbin /usr/local/var/homebrew /usr/local/var/homebrew/linked &&
+        chown -R ${whoami} /usr/local/Cellar /usr/local/Frameworks /usr/local/etc /usr/local/opt /usr/local/sbin /usr/local/var/homebrew /usr/local/var/homebrew/linked
+      `)
 
       return binPath
     } catch (error) {
